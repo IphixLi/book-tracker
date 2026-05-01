3ed020453a3d51e39d49f9f13e0f4ad1a6a62eea
IphixLi <iphigeniebera2024@u.northwestern.edu>
2026-04-30 20:37:43 -0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
   devIndicators: {
     appIsrStatus: false, // For newer versions of Next.js
     buildActivity: false, // To hide the build spinner
   },
+  output: "export",
+  basePath: "/book-tracker",
+  assetPrefix: "/book-tracker/",
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
