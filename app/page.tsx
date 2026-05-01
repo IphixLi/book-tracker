44a93b36c99a78dd5b7aa80a9fd21288c0a704c0
IphixLi <iphigeniebera2024@u.northwestern.edu>
2026-05-01 13:42:47 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { fetchAllShelves } from "@/lib/goodreads";
 export const revalidate = 3600;
 
 export default async function Home() {
-  const userId = process.env.GOODREADS_USER_ID;
+  const userId = "180395039";
 
   if (!userId) {
     throw new Error("Missing GOODREADS_USER_ID in environment variables");
