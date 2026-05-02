b0cd1655196ea4f21bc272978e863d1d2979998a
IphixLi <iphigeniebera2024@u.northwestern.edu>
2026-05-01 21:07:09 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import BooksView from "@/components/booksView";
 import { fetchAllShelves } from "@/lib/goodreads";
 
-export const revalidate = 3600;
-
 export default async function Home() {
   const userId = "180395039";
-
-  if (!userId) {
-    throw new Error("Missing GOODREADS_USER_ID in environment variables");
-  }
   const data = await fetchAllShelves(userId);
   return <BooksView data={data} />;
 }
